2778213d99f6d6ba5362113ab4c369c6911bd6db
Sonali Chouhan <119680970+Sonalichouhan@users.noreply.github.com>
2024-04-28 16:10:24 +0530

    Add files via upload

---
+++ Stackk.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class Stackk{
+    public static void main(String args[]){
+Stack<Integer> stack= new Stack<>();
+stack.push(4);
+stack.push(3);
+stack.push(2);
+stack.push(7);
+
+System.out.println(stack.pop());
+
+    }
+}
